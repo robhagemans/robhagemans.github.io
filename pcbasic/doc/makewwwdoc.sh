a434b9b3b65e0556122c34cd65db6707538b94fb
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-23 20:46:22 +0100

    sync documentation to project source updates

--- pcbasic/doc/makewwwdoc.sh
+++ pcbasic/doc/makewwwdoc.sh
@@ -1,5 +1,5 @@
 DOCLOC=../../../pc-basic/docsrc/
-VERSION=$(cat ../../../pc-basic/pcbasic/data/VERSION)
+VERSION=$(cat ../../../pc-basic/pcbasic/data/version.txt)
 (cat $DOCLOC/documentation.html; echo -e '<article>\n<h1 id="invocation">Invocation</h1>'; cat $DOCLOC/options.html $DOCLOC/examples.html; echo -e "</article>"; cat $DOCLOC/reference.html $DOCLOC/acknowledgements.html $DOCLOC/footer.html) > predoc.html
 $DOCLOC/maketoc.py predoc.html > toc.html
 (cat header.html; echo -e "<header>\n<h1>PC-BASIC $VERSION</h1>\n<p><small>Documentation compiled on $(date --utc).</small></p></header>"; cat toc.html predoc.html) > index.html
