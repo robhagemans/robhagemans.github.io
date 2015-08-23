9dfbb4fbf441c96d580059918b80b2b454c0de46
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-23 15:10:08 +0100

    amend makewwwdoc to new doc locations

--- pcbasic/doc/makewwwdoc.sh
+++ pcbasic/doc/makewwwdoc.sh
@@ -1,4 +1,6 @@
-(cat ../../../pc-basic/docsrc/documentation.html; echo -e '<article>\n<h1 id="invocation">Invocation</h1>'; cat ../../../pc-basic/docsrc/options.html ../../../pc-basic/docsrc/examples.html; echo -e "</article>"; cat ../../../pc-basic/docsrc/reference.html ../../../pc-basic/docsrc/acknowledgements.html ../../../pc-basic/docsrc/footer.html) > predoc.html
-../../../pc-basic/docsrc/maketoc.py predoc.html > toc.html
-(cat header.html; echo -e "<header>\n<h1>PC-BASIC $(cat ../../../pc-basic/data/VERSION)</h1>\n<p><small>Documentation compiled on $(date --utc).</small></p></header>"; cat toc.html predoc.html) > index.html
+DOCLOC=../../../pc-basic/docsrc/
+VERSION=$(cat ../../../pc-basic/pcbasic/data/VERSION)
+(cat $DOCLOC/documentation.html; echo -e '<article>\n<h1 id="invocation">Invocation</h1>'; cat $DOCLOC/options.html $DOCLOC/examples.html; echo -e "</article>"; cat $DOCLOC/reference.html $DOCLOC/acknowledgements.html $DOCLOC/footer.html) > predoc.html
+$DOCLOC/maketoc.py predoc.html > toc.html
+(cat header.html; echo -e "<header>\n<h1>PC-BASIC $VERSION</h1>\n<p><small>Documentation compiled on $(date --utc).</small></p></header>"; cat toc.html predoc.html) > index.html
 rm predoc.html toc.html
