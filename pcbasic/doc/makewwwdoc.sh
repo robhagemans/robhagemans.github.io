cec8863ece1304e4e980c94253a15b8e3491e826
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-08-27 15:38:26 +0100

    use source makedoc script to create docs

--- pcbasic/doc/makewwwdoc.sh
+++ pcbasic/doc/makewwwdoc.sh
@@ -1,7 +1,8 @@
-DOCLOC=../../../pc-basic/docsrc/
-VERSION=$(cat ../../../pc-basic/pcbasic/data/version.txt)
+BASE=../../../pc-basic/
+DOCSRCLOC=$BASE/docsrc/
+DOCLOC=$BASE/doc
+VERSION=$(cat $BASE/pcbasic/data/version.txt)
+pushd $DOCSRCLOC
+./makedoc.sh www/pcbasic/doc/header.html www/pcbasic/doc/index.html
+popd
 cp $DOCLOC/doc.css ../style
-(cat $DOCLOC/documentation.html; echo -e '<article>\n<h1 id="invocation">Invocation</h1>'; cat $DOCLOC/options.html $DOCLOC/examples.html; echo -e "</article>"; cat $DOCLOC/reference.html $DOCLOC/acknowledgements.html $DOCLOC/footer.html) > predoc.html
-$DOCLOC/maketoc.py predoc.html > toc.html
-(cat header.html; echo -e "<header>\n<h1>PC-BASIC $VERSION</h1>\n<p><small>Documentation compiled on $(date --utc).</small></p></header>"; cat toc.html predoc.html) > index.html
-rm predoc.html toc.html
