edd85ae0e3919c33fcc91be5f530e214b039d529
Rob Hagemans <robhagemans@yahoo.co.uk>
2017-04-02 12:03:41 +0100

    basicode test: update list of programs

--- basicode/buildlist.py
+++ basicode/buildlist.py
@@ -6,18 +6,19 @@ types = 'bc', 'bc2', 'bc3', 'b3c', 'bas', 'asc'
 
 print '<ol>'
 for root, dirs, files in sorted(os.walk('basicode/')):
-    print '      <li>' + root + '</li>'
-    print '      <ol>'
+    print '    <li>' + root
+    print '        <ol>'
     for name in sorted(files):
-        if [name.lower().endswith(t) for t in types]:
+        isbc = [name.lower().endswith(t) for t in types]
+        if True in isbc:
             with open(os.path.join(root, name), 'r') as f:
                 for line in f:
-                    if line.startswith('1000 '):
+                    if line.strip().startswith('1000 '):
                         try:
                             title = line.split('REM')[1].strip() or name
+                            title = ''.join(c for c in title if c.isalnum() or c in (' ', '-', '.', ',')).strip().title()
                         except IndexError:
                             title = name
-                        title = ''.join(c for c in title if c.isalnum() or c in (' ', '-', '.')).strip().title()
-                        print '        <li><a href="'+ os.path.join(root, name) + '" onclick="return loadFile(this);">' + title + '</a></li>'
-    print '      </ol>'
+                        print '            <li><a href="'+ os.path.join(root, name) + '" onclick="return loadFile(this);">' + title + '</a></li>'
+    print '        </ol>\n    </li>'
 print '</ol>'
