b1ccafb29068b8d44191b61db260a2bbaea020ed
Rob Hagemans <robhagemans@yahoo.co.uk>
2018-05-20 11:32:51 +0100

    include links to single-file & pdf docs as well as old versions

--- pcbasic/doc/2.0/makewwwdoc.py
+++ pcbasic/doc/2.0/makewwwdoc.py
@@ -4,5 +4,13 @@ here = os.path.dirname(os.path.realpath(os.path.abspath(__file__)))
 wd = os.path.join(here, '..', '..', '..', '..', 'pc-basic')
 sys.path.insert(0, wd)
 from docsrc.makedoc import makedoc
+# generate online doc
 makedoc(os.path.join(here, 'header.html'), os.path.join(here, 'index.html'), embedded_style=False)
+# generate offline doc
+makedoc()
+shutil.copy(os.path.join(wd, 'doc', 'PC-BASIC_documentation.html'), here)
+# generate PDF
+os.system('prince %s' % os.path.join(wd, 'doc', 'PC-BASIC_documentation.html'))
+shutil.copy(os.path.join(wd, 'doc', 'PC-BASIC_documentation.pdf'), here)
+# get styles
 shutil.copy(os.path.join(wd, 'docsrc', 'doc.css'), here)
